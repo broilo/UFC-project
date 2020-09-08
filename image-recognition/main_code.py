4c1c184b7e2d43bc2e17a225e0e43ad3bbd43c4a
Mateus Broilo <mateus.broilo90@gmail.com>
2020-09-08 09:50:09 -0300

    update

--- image-recognition/main_code.py
+++ image-recognition/main_code.py
@@ -1,16 +1,9 @@
-#import subprocess
-from function import load_pickle, two_fighters_accuracy, test_model_on_selected_photo
+import subprocess
+
 
 def main_runner():
-    #subprocess.call(["python", "function.py"])
-    
-    load_model, load_out_encoder = load_pickle()
+    subprocess.call(["python", "function.py"])
 
-    two_fighters_accuracy(load_model, load_out_encoder)
-
-    test_model_on_selected_photo(URL_TEST, load_model, load_out_encoder)
-
-    return
 
 if __name__ == "__main__":
     main_runner()
